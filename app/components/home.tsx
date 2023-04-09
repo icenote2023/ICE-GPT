f638fef37a4f9a0390a59e1ae2691e721aafe726
icenote2023 <125256128+icenote2023@users.noreply.github.com>
2023-04-09 17:45:30 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -119,9 +119,9 @@ function _Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>瑶绣AI</div>
           <div className={styles["sidebar-sub-title"]}>
-            Build your own AI assistant.
+            在这里了解瑶绣.
           </div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
